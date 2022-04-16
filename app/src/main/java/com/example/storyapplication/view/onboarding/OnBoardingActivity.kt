daa48a5724ccba7f692f2093581c7a8e3f1df3bc
abiyans_ <bagss69@gmail.com>
2022-04-16 07:48:44 +0700

    belummmm

---
+++ app/src/main/java/com/example/storyapplication/view/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,43 @@
+package com.example.storyapplication.view.onboarding
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.example.storyapplication.databinding.ActivityOnBoardingBinding
+import com.example.storyapplication.view.MainActivity
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnBoardingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.visitNow.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+
+        setupView()
+
+
+    }
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+}
