b204a56e768bf3a2ae9a73d2ce6133c289447119
abiyans_ <bagss69@gmail.com>
2022-05-15 14:49:16 +0700

    Subbmision 2 Paging Done

---
+++ app/src/main/java/com/example/storyapplication/database/UserStoryDao.kt
@@ -0,0 +1,20 @@
+package com.example.storyapplication.database
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.storyapplication.model.StoryModel
+
+@Dao
+interface UserStoryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUserStory(quote: List<StoryModel>)
+
+    @Query("SELECT * FROM user_story")
+    fun getAllUserStories(): PagingSource<Int, StoryModel>
+
+    @Query("DELETE FROM user_story")
+    suspend fun deleteAllUserStories()
+}
